a3181974ac3399112e30b25dbb68eef7b7d4ba07
Nishant Gaurav <140972654+nishant05gaurav@users.noreply.github.com>
2024-04-08 22:15:14 +0530

    first commit

---
+++ w3school_Problems/Question2.c
@@ -0,0 +1,46 @@
+/*
+Write a C program to print a block F using the hash (#), where the F has a height of six characters and width of five and four characters. And also print a very large 'C'.
+Expected Output:
+
+######
+#
+#
+#####
+#
+#
+#
+
+    ######
+  ##      ##
+ #
+ #
+ #
+ #
+ #
+  ##      ##
+    ######
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("######\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#####\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+
+    printf("   ######  \n");
+    printf(" ##      ##\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf(" ##      ##\n");
+    printf("   ######  \n");
+
+    return 0;
+}
