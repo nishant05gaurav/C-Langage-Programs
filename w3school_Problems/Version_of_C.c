212573afefede0a88698d48056018c0fd518649a
Nishant Gaurav <140972654+nishant05gaurav@users.noreply.github.com>
2024-04-26 01:29:33 +0530

    first commit

---
+++ w3school_Problems/Version_of_C.c
@@ -0,0 +1,19 @@
+
+// Question: Write a C program to get the C version you are using.
+
+// Solution:
+
+#include <stdio.h>
+
+int main()
+{
+    if (__STDC_VERSION__ >= 201710L)
+        printf("We are using C18!\n");
+    else if (__STDC_VERSION__ >= 201112L)
+        printf("We are using C11!\n");
+    else if (__STDC_VERSION__ >= 199901L)
+        printf("We are using C99!\n");
+    else
+        printf("We are using C89/C90!\n");
+    return 0;
+}
